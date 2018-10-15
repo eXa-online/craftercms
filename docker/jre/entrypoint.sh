955bdd878f185fd83ae30df46e9817b4b86d5cb1
Silke Jänichen <sjaenichen@exa-online.de>
2018-10-15 11:46:50 +0200

    create .ssh folder and copy key and known hosts from gitlab ci variables

--- docker/jre/entrypoint.sh
+++ docker/jre/entrypoint.sh
@@ -7,6 +7,11 @@ if [[ $# = 0 && -t 0 && -t 1 ]]; then
 else
     if [ "$1" = 'supervisord' ]; then
         chown -R "$CRAFTER_USER" "$CRAFTER_ROOT/data"
+        mkdir -p "$CRAFTER_ROOT"/.ssh
+        chmod 700 "$CRAFTER_ROOT"/.ssh
+        echo "$SSH_KNOWN_HOSTS" > "$CRAFTER_ROOT"/.ssh/known_hosts
+        echo "$SSH_PRIVATE_KEY"| tr -d '\r'  > "$CRAFTER_ROOT"/.ssh/id_rsa
+        chmod 600 "$CRAFTER_ROOT"/.ssh/known_hosts "$CRAFTER_ROOT"/.ssh/id_rsa
 
         exec /usr/bin/supervisord -n -c /etc/supervisor/supervisord.conf
     else
