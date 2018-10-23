bc8cac6bab38576b8fd01c199fa457bffab06e12
Silke Jänichen <sjaenichen@exa-online.de>
2018-10-23 15:09:19 +0200

    Adapt deployment process for docker images: - share logs folder to host - copy ssh information into docker container

--- docker/jre/entrypoint.sh
+++ docker/jre/entrypoint.sh
@@ -5,14 +5,11 @@ set -e
 if [[ $# = 0 && -t 0 && -t 1 ]]; then
     exec bash
 else
-    if [ "$1" = 'supervisord' ]; then
-        chown -R "$CRAFTER_USER" "$CRAFTER_ROOT/data"
-        mkdir -p "$CRAFTER_ROOT"/.ssh
-        chmod 700 "$CRAFTER_ROOT"/.ssh
-        echo "$SSH_KNOWN_HOSTS" > "$CRAFTER_ROOT"/.ssh/known_hosts
-        echo "$SSH_PRIVATE_KEY"| tr -d '\r'  > "$CRAFTER_ROOT"/.ssh/id_rsa
-        chmod 600 "$CRAFTER_ROOT"/.ssh/known_hosts "$CRAFTER_ROOT"/.ssh/id_rsa
+    chown -R "$CRAFTER_USER:$CRAFTER_USER" "$CRAFTER_ROOT/data"
+    chown -R "$CRAFTER_USER:$CRAFTER_USER" "$CRAFTER_ROOT/logs"
+    chown -R "$CRAFTER_USER:$CRAFTER_USER" "$CRAFTER_ROOT/.ssh"
 
+    if [ "$1" = 'supervisord' ]; then
         exec /usr/bin/supervisord -n -c /etc/supervisor/supervisord.conf
     else
         exec "$@"
